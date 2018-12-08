765fb3b1df0386890cdebe2d5cc6ae65c2901305
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-09 00:16:15 +0100

    Add static method create in Island class

--- src/main/java/fr/pumpmyskycore/Island.java
+++ src/main/java/fr/pumpmyskycore/Island.java
@@ -1,5 +1,12 @@
 package fr.pumpmyskycore;
 
+import java.util.UUID;
+
 public class Island {
+
+	public static Island create(UUID uniqueId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
