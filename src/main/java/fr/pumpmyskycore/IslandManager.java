2c63fb62d4c1d12319878dae8ed58a97bfaf7878
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-08 16:53:39 +0100

    Add constants

--- src/main/java/fr/pumpmyskycore/IslandManager.java
+++ src/main/java/fr/pumpmyskycore/IslandManager.java
@@ -1,12 +1,25 @@
 package fr.pumpmyskycore;
 
+import java.io.File;
+
 public class IslandManager {
 
 	public abstract class IslandConstant {
 		
 		public static final int ISLAND_SIZE = 4096; // 16 * 256 chunk per island
 		public static final int ISLAND_SIDE_NUM = 80; 	// 80*80 = 6400 islands
+		public static final String ISLAND_FOLDER_NAME = "islands";
 		
 	}
+
+	public static void initIslandFolder(File f) {
+		
+		if(!f.exists()) {
+			f.mkdir();
+		}
+		
+	}
+	
+	
 	
 }
