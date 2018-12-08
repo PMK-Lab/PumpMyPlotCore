34c7e475d660382ec58c6945cb027c81dc03bd9d
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-08 11:49:53 +0100

    Add constant data in intra class IslandManager

--- src/main/java/fr/pumpmyskycore/IslandManager.java
+++ src/main/java/fr/pumpmyskycore/IslandManager.java
@@ -2,4 +2,11 @@ package fr.pumpmyskycore;
 
 public class IslandManager {
 
+	public abstract class IslandConstant {
+		
+		public static final int ISLAND_SIZE = 4096; // 16 * 256 chunk per island
+		public static final int ISLAND_SIDE_NUM = 80; 	// 80*80 = 6400 islands
+		
+	}
+	
 }
