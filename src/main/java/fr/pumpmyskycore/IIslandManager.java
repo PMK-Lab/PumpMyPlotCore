ce119a556d032449db5fe6e04035099151a01d2d
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-09 00:26:47 +0100

    Update playerGetISland & add throws PlayerDoesNotHaveIslandException

--- src/main/java/fr/pumpmyskycore/IIslandManager.java
+++ src/main/java/fr/pumpmyskycore/IIslandManager.java
@@ -3,16 +3,17 @@ package fr.pumpmyskycore;
 import java.io.File;
 
 import fr.pumpmyskycore.exceptions.PlayerAlreadyHaveIslandException;
+import fr.pumpmyskycore.exceptions.PlayerDoesNotHaveIslandException;
 
 public interface IIslandManager<T> {
 	
-	public Island createIsland(T player) throws PlayerAlreadyHaveIslandException;
+	public Island createIsland(T player) throws PlayerAlreadyHaveIslandException, PlayerDoesNotHaveIslandException;
 	
 	public Island getIsland(File f);
 	
 	public Island getIsland(IslandLocation l);
 	
-	public Island playerGetIsland(T player);
+	public Island playerGetIsland(T player) throws PlayerDoesNotHaveIslandException;
 	
 	public void playerAddIsland( Island island, T player);
 	
