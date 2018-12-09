5ec4da5d1ebcd02de53a23c94eba571541e497fb
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-09 14:51:52 +0100

    Add final constant ISLAND_INDEX_FILE_NAME

--- src/main/java/fr/pumpmyskycore/IslandManager.java
+++ src/main/java/fr/pumpmyskycore/IslandManager.java
@@ -11,6 +11,8 @@ public abstract class IslandManager<T> implements IIslandManager<T>{
 		public static final int ISLAND_SIZE = 4096; // 16 * 256 chunk per island
 		public static final int ISLAND_SIDE_NUM = 80; 	// 80*80 = 6400 islands
 		public static final String ISLAND_FOLDER_NAME = "islands";
+		public static final String ISLAND_INDEX_FILE_NAME = "islands.yml";
+		
 		
 	}
 	
