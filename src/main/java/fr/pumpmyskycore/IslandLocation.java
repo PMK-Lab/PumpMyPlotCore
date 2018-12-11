6a9116bac9fd79def780d769d980accbad352528
Clem Fern <clement.coussieu@gmail.com>
2018-12-11 11:45:47 +0100

    Update IslandLocation

--- src/main/java/fr/pumpmyskycore/IslandLocation.java
+++ src/main/java/fr/pumpmyskycore/IslandLocation.java
@@ -2,4 +2,27 @@ package fr.pumpmyskycore;
 
 public class IslandLocation {
 
+	public static final String X_STRING = ".x";
+	public static final String Y_STRING = ".y";
+	
+	private String x;
+	private String y;
+	
+	public IslandLocation(String x, String y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String getX() {
+		
+		return this.x;
+		
+	}
+	
+	public String getY() {
+		
+		return this.y;
+		
+	}
+
 }
