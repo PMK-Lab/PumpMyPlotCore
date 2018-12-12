739e7a973f8611d64918b8574e25e821b763c7d7
iutuser <iutuser@DESKTOP-O0NT4PA>
2018-12-12 15:05:29 +0100

    Add junit 5 test plugin in pom.xml

---
+++ src/test/java/pumpmyskycore/IslandTest.java
@@ -0,0 +1,16 @@
+package pumpmyskycore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IslandTest {
+
+	@Test
+	public void createIslandTest() {
+		
+		assertEquals(true, true);
+		
+	}
+	
+}
