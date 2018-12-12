abf3edc714af5bfbce31c060786031e3694b0b7a
Clem-Fern <clement.coussieu@gmail.com>
2018-12-12 20:33:17 +0100

    Create & add IslandHome class

---
+++ src/main/java/fr/pumpmyskycore/IslandHome.java
@@ -0,0 +1,29 @@
+package fr.pumpmyskycore;
+
+public class IslandHome {
+
+	private double x;
+	private double y;
+	private double z;
+	
+	public IslandHome(double x, double y, double z) {
+		
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+	
+}
