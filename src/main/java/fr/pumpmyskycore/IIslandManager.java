93b78ff74d39b1f41d267f400ea12c8a94d04f86
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-31 15:35:15 +0100

    Update useless methods declaration in IIslandManager interface

--- src/main/java/fr/pumpmyskycore/IIslandManager.java
+++ src/main/java/fr/pumpmyskycore/IIslandManager.java
@@ -2,25 +2,8 @@ package fr.pumpmyskycore;
 
 import java.util.UUID;
 
-import fr.pumpmyskycore.exceptions.IslandIsNotEmptyException;
-import fr.pumpmyskycore.exceptions.PlayerAlreadyHaveIslandException;
-import fr.pumpmyskycore.exceptions.PlayerDoesNotHaveIslandException;
-import fr.pumpmyskycore.exceptions.RestrictActionToOwnerIslandException;
-
 public interface IIslandManager<T> {
 	
 	public UUID getMinecraftUUID(T player);
-	
-	public boolean playerHasIsland(T player);
-	
-	public Island playerCreateIsland(T player) throws PlayerAlreadyHaveIslandException, PlayerDoesNotHaveIslandException, IOException;
-	
-	public Island playerGetIsland(T player) throws PlayerDoesNotHaveIslandException;
-	
-	public boolean playerLeaveIsland(T player) throws PlayerDoesNotHaveIslandException, IslandIsNotEmptyException, IOException;
-	
-	public void playerInviteIsland(T islandOwner, T player) throws PlayerDoesNotHaveIslandException, RestrictActionToOwnerIslandException;
-	
-	public void playerUninviteIsland(T islandOwner, T player) throws PlayerDoesNotHaveIslandException, RestrictActionToOwnerIslandException;
 	
 }
