00cd1b2542da9a87d276ccc93ea9543d319ef745
clem <clem@PC-CLEM>
2019-01-02 01:41:49 +0100

    Create IslandLocationParsingException class

---
+++ src/main/java/fr/pumpmyskycore/exceptions/IslandLocationParsingException.java
@@ -0,0 +1,12 @@
+package fr.pumpmyskycore.exceptions;
+
+public class IslandLocationParsingException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2531969246057424467L;
+	
+	
+
+}
