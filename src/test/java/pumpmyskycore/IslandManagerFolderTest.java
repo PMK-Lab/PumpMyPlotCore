9ee56414479ad9afbaa73d5cfd00847395b0ee66
Clem-Fern-Dev <clement.coussieu@gmail.com>
2019-01-04 20:37:14 +0100

    Update import in IslandManagerFolderTest class

--- src/test/java/pumpmyskycore/IslandManagerFolderTest.java
+++ src/test/java/pumpmyskycore/IslandManagerFolderTest.java
@@ -1,6 +1,6 @@
 package pumpmyskycore;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
