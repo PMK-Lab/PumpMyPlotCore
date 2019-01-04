44d8701ffdff9952baf622849006e9a48521ff09
Clem-Fern-Dev <clement.coussieu@gmail.com>
2019-01-04 17:17:22 +0100

    Add utils fields & init method in IslandIndexTest class

--- src/test/java/pumpmyskycore/IslandIndexTest.java
+++ src/test/java/pumpmyskycore/IslandIndexTest.java
@@ -1,7 +1,33 @@
 package pumpmyskycore;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import fr.pumpmyskycore.IslandIndex;
+
+@TestInstance(Lifecycle.PER_CLASS)
 public class IslandIndexTest {
 	
+	private IslandIndex index;
+	private File file;
 	
+	public void init() throws IOException, InvalidConfigurationException {
+		
+		this.file = new File(this.getClass().getSimpleName() + "_" + System.currentTimeMillis());
+		this.index = IslandIndex.init(this.file.toPath());
+		
+	}
+	
+	@Test
+	public void correctlyInitialize() {
+		
+		
+		
+	}
 	
 }
