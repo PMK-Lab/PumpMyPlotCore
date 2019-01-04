611cae63885970a561a852b8f52ae9ca3338ef22
Clem-Fern-Dev <clement.coussieu@gmail.com>
2019-01-04 20:38:32 +0100

    Update testings methods in IslandIndexTest class

--- src/test/java/pumpmyskycore/IslandIndexTest.java
+++ src/test/java/pumpmyskycore/IslandIndexTest.java
@@ -21,20 +21,58 @@ import pumpmyskycore.utils.TestIslandManager;
 @TestInstance(Lifecycle.PER_CLASS)
 public class IslandIndexTest {
 	
-	private IslandIndex index;
 	private File file;
 	
+	@BeforeAll
 	public void init() throws IOException, InvalidConfigurationException {
 		
-		this.file = new File(this.getClass().getSimpleName() + "_" + System.currentTimeMillis());
-		this.index = IslandIndex.init(this.file.toPath());
+		this.file = new File("TEST" + File.separator + this.getClass().getSimpleName() + "_" + System.currentTimeMillis());
 		
 	}
 	
 	@Test
-	public void correctlyInitialize() {
+	public void returnCorrectIslandLocatioonOnCreatingFirstFreeLocFile() throws IOException, InvalidConfigurationException {
 		
+		TestIslandManager manager = new TestIslandManager(this.file);
 		
+		IslandLocation loc = null;
+		
+		for (int x = 1; x <= 3 ; x++) {
+			
+			for (int y = 1; y <= IslandManagerConstant.ISLAND_SIDE_NUM ; y++) {
+				
+				loc = manager.getIslandIndex().createFirstFreeLocFile(manager.getIslandPath());				
+				System.out.println(x + ":" + y + "\t" + loc);
+				
+				assertEquals(loc.getX(),x);
+				assertEquals(loc.getY(),y);
+				
+				File f = new File(manager.getIslandPath() + File.separator + x + File.separator + y);
+				assertTrue(f.exists());
+				
+			}
+			System.out.println("");
+		}
+		
+		assertFalse(loc == null);
+		
+		File f = new File(manager.getIslandPath() + File.separator + 2 + File.separator + 50);
+		assertTrue(f.delete());
+		
+		loc = manager.getIslandIndex().createFirstFreeLocFile(manager.getIslandPath());				
+		System.out.println(2 + ":" + 50 + "\t" + loc);
+		
+		assertEquals(loc.getX(),2);
+		assertEquals(loc.getY(),50);		
+		
+	}
+	
+	//@Test
+	public void initIndexSection() throws IOException, InvalidConfigurationException {
+		
+		IslandIndex index = IslandIndex.init(file.toPath());
+		
+		assertTrue(index.getYaml().isConfigurationSection("islands"));
 		
 	}
 	
