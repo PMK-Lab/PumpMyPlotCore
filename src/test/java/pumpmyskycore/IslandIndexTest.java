d981b7d2bcf9ded4f705d326a3fe06b015231559
Clem-Fern-Dev <clement.coussieu@gmail.com>
2019-01-04 20:37:54 +0100

    Update import in IslandIndexTest*

--- src/test/java/pumpmyskycore/IslandIndexTest.java
+++ src/test/java/pumpmyskycore/IslandIndexTest.java
@@ -1,14 +1,22 @@
 package pumpmyskycore;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
 
 import org.bukkit.configuration.InvalidConfigurationException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import fr.pumpmyskycore.IslandIndex;
+import fr.pumpmyskycore.IslandLocation;
+import fr.pumpmyskycore.IslandManager.IslandManagerConstant;
+import pumpmyskycore.utils.TestIslandManager;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class IslandIndexTest {
