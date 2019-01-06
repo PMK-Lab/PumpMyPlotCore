9932a29de8ae66316ba96399877a076f60414cc7
Clem-Fern <clement.coussieu@gmail.com>
2019-01-06 01:23:34 +0100

    Adding testing methods in IslandManagerPlayerInvitesActionTest class

--- src/test/java/pumpmyskycore/IslandManagerPlayerInvitesActionTest.java
+++ src/test/java/pumpmyskycore/IslandManagerPlayerInvitesActionTest.java
@@ -1,5 +1,36 @@
 package pumpmyskycore;
 
+import java.io.IOException;
+
+import fr.pumpmyskycore.Island;
+import fr.pumpmyskycore.exceptions.PlayerAlreadyHaveIslandException;
+import fr.pumpmyskycore.exceptions.PlayerAlreadyInvited;
+import fr.pumpmyskycore.exceptions.PlayerDoesNotHaveIslandException;
+import fr.pumpmyskycore.exceptions.PlayerDoesNotInvited;
+import fr.pumpmyskycore.exceptions.RestrictActionToOwnerIslandException;
+
 public class IslandManagerPlayerInvitesActionTest {
 
+	public void playerInviteIsland() {
+		
+		
+		
+	}
+	
+	public void playerUninviteIsland() {
+		
+		
+		
+	}
+	
+	public void playerAcceptInviteIsland() {
+		
+		
+		
+	}
+	
+	public void playerRefuseInviteIsland() {
+		
+	}
+	
 }
