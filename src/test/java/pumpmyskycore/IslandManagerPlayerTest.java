20b560480b7828b5d93b29e85f9189732651a26b
Clem-Fern <clement.coussieu@gmail.com>
2019-01-06 01:17:30 +0100

    Update import in IslandManagerPlayerTest class

--- src/test/java/pumpmyskycore/IslandManagerPlayerTest.java
+++ src/test/java/pumpmyskycore/IslandManagerPlayerTest.java
@@ -1,14 +1,17 @@
 package pumpmyskycore;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.UUID;
 
 import org.bukkit.configuration.InvalidConfigurationException;
-import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.api.Test;
 
 import fr.pumpmyskycore.Island;
+import fr.pumpmyskycore.exceptions.IslandIsNotEmptyException;
 import fr.pumpmyskycore.exceptions.PlayerAlreadyHaveIslandException;
 import fr.pumpmyskycore.exceptions.PlayerDoesNotHaveIslandException;
 import pumpmyskycore.utils.FakePlayer;
