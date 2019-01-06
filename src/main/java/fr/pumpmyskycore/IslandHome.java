3e291992290bd94d8cd82a8bca3daaf101a8d725
Clem-Fern <clement.coussieu@gmail.com>
2019-01-07 00:46:22 +0100

    Add fields & getters in IslandHome class

--- src/main/java/fr/pumpmyskycore/IslandHome.java
+++ src/main/java/fr/pumpmyskycore/IslandHome.java
@@ -2,4 +2,18 @@ package fr.pumpmyskycore;
 
 public class IslandHome {
 
+	private double x;
+	private double y;
+	private double z;
+	
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public double getZ() {
+		return z;
+	}
+	
 }
