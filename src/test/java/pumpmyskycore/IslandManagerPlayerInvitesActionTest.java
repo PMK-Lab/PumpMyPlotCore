bbc5c32860654f563a84ec511d0042b60ed2aa69
Clem-Fern <clement.coussieu@gmail.com>
2019-01-06 01:24:07 +0100

    Update import in Player Invites testing class

--- src/test/java/pumpmyskycore/IslandManagerPlayerInvitesActionTest.java
+++ src/test/java/pumpmyskycore/IslandManagerPlayerInvitesActionTest.java
@@ -1,14 +1,5 @@
 package pumpmyskycore;
 
-import java.io.IOException;
-
-import fr.pumpmyskycore.Island;
-import fr.pumpmyskycore.exceptions.PlayerAlreadyHaveIslandException;
-import fr.pumpmyskycore.exceptions.PlayerAlreadyInvited;
-import fr.pumpmyskycore.exceptions.PlayerDoesNotHaveIslandException;
-import fr.pumpmyskycore.exceptions.PlayerDoesNotInvited;
-import fr.pumpmyskycore.exceptions.RestrictActionToOwnerIslandException;
-
 public class IslandManagerPlayerInvitesActionTest {
 
 	public void playerInviteIsland() {
