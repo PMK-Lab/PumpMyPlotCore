b1e58ed50eeb7a7e77673e0b7eb4bbb81cb7c71a
Clem-Fern <clement.coussieu@gmail.com>
2019-01-07 00:25:04 +0100

    Create Player is not member Island Exception class

---
+++ src/main/java/fr/pumpmyskycore/PlayerIsNotMemberIslandException.java
@@ -0,0 +1,34 @@
+package fr.pumpmyskycore;
+
+import java.util.UUID;
+
+public class PlayerIsNotMemberIslandException extends Exception {
+
+	private UUID uuid;
+	private Island island;
+
+	public PlayerIsNotMemberIslandException(UUID uuid, Island island) {
+		
+		this.uuid = uuid;
+		this.island = island;
+		
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public Island getIsland() {
+		return island;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5532131247089794958L;
+
+}
