c217d8fe3ee340420699a75529ed66cee9d0387f
Clem-Fern <clement.coussieu@gmail.com>
2019-01-07 21:48:03 +0100

    Update import in PlayerCreatePlotTest class

--- src/test/java/pumpmyskycore/player/PlayerCreatePlotTest.java
+++ src/test/java/pumpmyskycore/player/PlayerCreatePlotTest.java
@@ -1,5 +1,22 @@
 package pumpmyskycore.player;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import fr.pumpmyskycore.Plot;
+import fr.pumpmyskycore.exceptions.PlayerAlreadyHavePlotException;
+import fr.pumpmyskycore.exceptions.PlayerDoesNotHavePlotException;
+import pumpmyskycore.utils.FakePlayer;
+import pumpmyskycore.utils.TestPlotManager;
+
 public class PlayerCreatePlotTest {
 
 }
