774340552d9f0290fa03dff21d2d6317bdc25b06
Clem-Fern <clement.coussieu@gmail.com>
2019-01-09 19:14:40 +0100

    Update import in PlayerSetHomePlotTest class

--- src/test/java/pumpmyskycore/player/PlayerSetHomePlotTest.java
+++ src/test/java/pumpmyskycore/player/PlayerSetHomePlotTest.java
@@ -1,5 +1,27 @@
 package pumpmyskycore.player;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import fr.pumpmyplotcore.Plot;
+import fr.pumpmyplotcore.PlotHome;
+import fr.pumpmyplotcore.PlotManager.PlotManagerConstant;
+import fr.pumpmyplotcore.exceptions.InvalidePlotHomeLocationException;
+import fr.pumpmyplotcore.exceptions.PlayerAlreadyHavePlotException;
+import fr.pumpmyplotcore.exceptions.PlayerAlreadyInvitedPlotException;
+import fr.pumpmyplotcore.exceptions.PlayerDoesNotHavePlotException;
+import fr.pumpmyplotcore.exceptions.PlayerDoesNotInvitedPlotException;
+import fr.pumpmyplotcore.exceptions.RestrictActionToPlotOwnerException;
+import pumpmyskycore.utils.FakePlayer;
+import pumpmyskycore.utils.TestPlotManager;
+
 public class PlayerSetHomePlotTest {
 
 }
