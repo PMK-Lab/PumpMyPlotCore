664064ee90929b8c35e291d98a3ebe221f1e09c4
Clem-Fern <clement.coussieu@gmail.com>
2019-01-09 13:05:39 +0100

    Update import in PlayerRefuseInvitesPlotTest class

--- src/test/java/pumpmyskycore/player/invites/PlayerRefuseInvitesPlotTest.java
+++ src/test/java/pumpmyskycore/player/invites/PlayerRefuseInvitesPlotTest.java
@@ -1,5 +1,25 @@
 package pumpmyskycore.player.invites;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import fr.pumpmyplotcore.Plot;
+import fr.pumpmyplotcore.exceptions.PlayerAlreadyHavePlotException;
+import fr.pumpmyplotcore.exceptions.PlayerAlreadyInvitedPlotException;
+import fr.pumpmyplotcore.exceptions.PlayerDoesNotHavePlotException;
+import fr.pumpmyplotcore.exceptions.PlayerDoesNotInvitedPlotException;
+import fr.pumpmyplotcore.exceptions.RestrictActionToPlotOwnerException;
+import pumpmyskycore.utils.FakePlayer;
+import pumpmyskycore.utils.TestPlotManager;
+
 public class PlayerRefuseInvitesPlotTest {
 
 	public void playerRefuseInviteIsland() {
