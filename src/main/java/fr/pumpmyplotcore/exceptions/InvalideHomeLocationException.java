921dcc4354b322e7fb422283ce1bdcc449c3e9c3
Clem-Fern <clement.coussieu@gmail.com>
2019-01-09 13:14:04 +0100

    Add fields/constructor/getter in InvalideHomeLocationException class

--- src/main/java/fr/pumpmyplotcore/exceptions/InvalideHomeLocationException.java
+++ src/main/java/fr/pumpmyplotcore/exceptions/InvalideHomeLocationException.java
@@ -5,4 +5,31 @@ import fr.pumpmyplotcore.PlotHome;
 
 public class InvalideHomeLocationException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8545358645206896623L;
+
+	private Plot plot;
+	private PlotHome loc;
+	
+	public InvalideHomeLocationException(Plot plot, PlotHome loc) {
+		
+		this.plot = plot;
+		this.loc = loc;
+		
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Plot getPlot() {
+		return plot;
+	}
+
+	public PlotHome getLoc() {
+		return loc;
+	}
+
 }
